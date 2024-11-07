1bd85e982c8fabea23bf1a7e8012b4f22afe56ca
panjianugraah <220711843@students.uajy.ac.id>
2024-11-07 10:35:59 +0700

    Update home

--- lib/home.dart
+++ lib/home.dart
@@ -19,29 +19,40 @@ class _HomeViewState extends State<HomeView> {
   }
 
   static const List<Widget> _widgetOptions = <Widget>[
+    HomeContent(),
     Center(
-        child: Image(image: NetworkImage('https://picsum.photos/1000/1920'))),
-  
-   ListNamaView(),
-   ProfileScreen(),
+      child: Image(image: NetworkImage('https://picsum.photos/1000/1920')),
+    ),
+    Center(
+      child: Text("Test"),
+    ),
+    // Assuming these are other views you want to include
+    ProfileScreen(),
+
+    // Add any additional views here as needed
   ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       bottomNavigationBar: BottomNavigationBar(
-        selectedItemColor: Colors.blue,
+        selectedItemColor: Colors.blue.shade900,
         unselectedItemColor: Colors.grey,
         iconSize: 40,
         showUnselectedLabels: false,
+        backgroundColor: Colors.blue.shade900,
         items: const [
           BottomNavigationBarItem(
             icon: Icon(Icons.home),
             label: 'Home',
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.list),
-            label: 'List',
+            icon: Icon(Icons.edit_document), // Set icon color to white
+            label: 'Cetak',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.timelapse_rounded), // Set icon color to white
+            label: 'History',
           ),
           BottomNavigationBarItem(
             icon: Icon(Icons.person),
@@ -49,9 +60,190 @@ class _HomeViewState extends State<HomeView> {
           ),
         ],
         currentIndex: _selectedIndex,
-        onTap: _onItemTapped, 
+        onTap: _onItemTapped,
       ),
       body: _widgetOptions.elementAt(_selectedIndex),
+    );
+  }
+}
+
+class HomeContent extends StatelessWidget {
+  const HomeContent({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          // Logo and Search Form
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              children: [
+                Image.network(
+                  '',
+                  height: 80,
+                ),
+                const SizedBox(height: 16),
+                Container(
+                  padding: const EdgeInsets.all(16.0),
+                  decoration: BoxDecoration(
+                    color: Colors.blue.shade900,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Column(
+                    children: [
+                      Row(
+                        children: [
+                          Expanded(
+                            child: TextField(
+                              decoration: InputDecoration(
+                                hintText: 'Dari',
+                                filled: true,
+                                fillColor: Colors.white,
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(8),
+                                ),
+                              ),
+                            ),
+                          ),
+                          const SizedBox(width: 8),
+                          Expanded(
+                            child: TextField(
+                              decoration: InputDecoration(
+                                hintText: 'Tujuan',
+                                filled: true,
+                                fillColor: Colors.white,
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(8),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(height: 8),
+                      Row(
+                        children: [
+                          Expanded(
+                            child: TextField(
+                              decoration: InputDecoration(
+                                hintText: 'Tanggal Berangkat',
+                                filled: true,
+                                fillColor: Colors.white,
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(8),
+                                ),
+                                prefixIcon: Icon(Icons.calendar_today),
+                              ),
+                            ),
+                          ),
+                          const SizedBox(width: 8),
+                          Expanded(
+                            child: TextField(
+                              decoration: InputDecoration(
+                                hintText: 'Kursi',
+                                filled: true,
+                                fillColor: Colors.white,
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(8),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(height: 16),
+                      ElevatedButton(
+                        onPressed: () {},
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.yellow,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(8),
+                          ),
+                        ),
+                        child: const Text('Cari',
+                            style: TextStyle(color: Colors.black)),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          // Promo Section
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text(
+                  'Promo',
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                ),
+                const SizedBox(height: 8),
+                Row(
+                  children: [],
+                ),
+              ],
+            ),
+          ),
+          // Tips Traveling Section
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text(
+                  'Tips Traveling',
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                ),
+                const SizedBox(height: 8),
+                Row(
+                  children: [],
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class PromoCard extends StatelessWidget {
+  final String imageUrl;
+  final String? discount;
+
+  const PromoCard({super.key, required this.imageUrl, this.discount});
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        ClipRRect(
+          borderRadius: BorderRadius.circular(8),
+          child: Image.network(
+            imageUrl,
+            height: 100,
+            width: double.infinity,
+            fit: BoxFit.cover,
+          ),
+        ),
+        if (discount != null)
+          Positioned(
+            left: 8,
+            top: 8,
+            child: Container(
+              padding: const EdgeInsets.all(4.0),
+              color: Colors.black54,
+              child: Text(
+                'Diskon $discount',
+                style: const TextStyle(color: Colors.white),
+              ),
+            ),
+          ),
+      ],
     );
   }
 }
