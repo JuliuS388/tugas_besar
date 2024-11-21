5515e1fb8677474dceb9707858ae8088dd41810f
Julius Aditya <220711892@students.uajy.ac.id>
2024-11-21 08:23:56 +0700

    test

--- lib/main.dart
+++ lib/main.dart
@@ -12,9 +12,7 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false, // Menghilangkan banner debug
-      home: DetailBus(), // Hapus 'const' di sini
+     // Hapus 'const' di sini
     return const MaterialApp(
       home: LoginView(),
     );
