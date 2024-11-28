3472b44dacaa23a7cf9c04f395722c9ad258624f
Julius Aditya <220711892@students.uajy.ac.id>
2024-11-28 19:52:13 +0700

    PDF

--- lib/main.dart
+++ lib/main.dart
@@ -9,30 +9,6 @@ void main() {
   runApp(MainApp());
 }
 
-// class MainApp extends StatelessWidget {
-//   const MainApp({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//      // Hapus 'const' di sini
-//     return const MaterialApp(
-//       home: TicketPreview(),
-//     );
-//   }
-// }
-
-// class MainApp extends StatelessWidget {
-//   const MainApp({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       debugShowCheckedModeBanner: false,
-//       home: TicketPreview(), 
-//     );
-//   }
-// }
-
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
